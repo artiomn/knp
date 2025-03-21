2caa87b3e0049471f9b3a96b4b4e1b96df97c0b7
a_vartenkov <Andrey.Vartenkov@kaspersky.com>
2025-03-21 14:58:54 +0300

    Add licenses to all files

--- examples/mnist-learn/main.cpp
+++ examples/mnist-learn/main.cpp
@@ -1,6 +1,23 @@
-//
-// Created by an_vartenkov on 30.08.24.
-//
+/**
+ * @file main.cpp
+ * @brief Example of training a MNIST network
+ * @kaspersky_support A. Vartenkov
+ * @date 30.08.2024
+ * @license Apache 2.0
+ * @copyright © 2024 AO Kaspersky Lab
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 #include <knp/core/projection.h>
 #include <knp/framework/model.h>
 #include <knp/framework/model_executor.h>
