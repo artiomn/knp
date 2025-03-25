fabe959280c0dbba6e3ac2d6fecdea1090edab65
Артём <artiom14@yandex.ru>
2025-03-25 15:55:01 +0300

    Update run_pvs.sh

--- ci/run_pvs.sh
+++ ci/run_pvs.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -x
 
-PVS_LICENSE="${1}"
-BUILD_DIR="${2:-build}"
+PVS_LICENSE="${1} ${2}"
+BUILD_DIR="${3:-build}"
 
 pvs-studio-analyzer credentials ${PVS_LICENSE}
 cmake --build "${BUILD_DIR}" --parallel --target pvs-analyze
