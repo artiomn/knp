98e030d26f88f968e456c4e1fed8f9211c65c08a
Artiom N. <artiom@artiom>
2026-04-21 01:04:10 +0300

    Base Docker image fixed: #21

--- docker/base-image/start.sh
+++ docker/base-image/start.sh
@@ -13,21 +13,31 @@ fi
 echo "[WARN] CUDA not found. Installing..."
 
 function install_cuda_debian() {
-    echo "[INFO] Installing CUDA for Debian..."
+    export DEBIAN_VERSION=debian$(cat /etc/debian_version | sed 's/\..*//')
+    echo "[INFO] Installing CUDA for ${DEBIAN_VERSION}..."
 
-    wget https://developer.download.nvidia.com/compute/cuda/repos/debian11/x86_64/cuda-keyring_1.1-1_all.deb \
+    # cuda-keyring package adds correct repository.
+    curl -fS https://developer.download.nvidia.com/compute/cuda/repos/${DEBIAN_VERSION}/x86_64/cuda-keyring_1.1-1_all.deb -o cuda-keyring_1.1-1_all.deb \
     && dpkg -i cuda-keyring_1.1-1_all.deb \
+    && rm -f cuda-keyring_1.1-1_all.deb \
+    && curl -sSL "https://keyserver.ubuntu.com/pks/lookup?op=get&search=0xA4B469963BF863CC" | \
+       gpg --dearmor | tee /etc/apt/keyrings/cuda.gpg > /dev/null \
     && apt-get update \
-    && apt-get install -y cuda-toolkit
+    && apt-get install -y cuda-toolkit-12.9
 
     echo "[INFO] CUDA installation complete."
 }
 
-install_cuda_debian
 
-if command -v nvcc >/dev/null 2>&1; then
-    nvcc --version
+if [ "x$(uname -m)" = "xx86_64" ]; then
+    install_cuda_debian
+
+    if command -v nvcc >/dev/null 2>&1; then
+        nvcc --version
+    else
+        echo "[ERROR] CUDA installation failed"
+        exit 1
+    fi
 else
-    echo "[ERROR] CUDA installation failed"
-    exit 1
+    echo "Unsupported platform for CUDA: ${TARGETPLATFORM}"
 fi
