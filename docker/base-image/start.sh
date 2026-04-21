8d37d5137d55e56a2f5ff7be39fe2cf17db18e5c
Artiom N. <artiom@artiom>
2026-04-22 02:10:49 +0300

    CUDA installation script fixed: #21

--- docker/base-image/start.sh
+++ docker/base-image/start.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+CUDA_VERSION="12.9"
+
 echo "[INFO] Checking for CUDA..."
 
 if command -v nvcc >/dev/null 2>&1; then
@@ -16,6 +18,8 @@ function install_cuda_debian() {
     export DEBIAN_VERSION=debian$(cat /etc/debian_version | sed 's/\..*//')
     echo "[INFO] Installing CUDA for ${DEBIAN_VERSION}..."
 
+    CUDA_PKG_VERSION=$(echo ${CUDA_VERSION}|tr '.' '-')
+
     # cuda-keyring package adds correct repository.
     curl -fS https://developer.download.nvidia.com/compute/cuda/repos/${DEBIAN_VERSION}/x86_64/cuda-keyring_1.1-1_all.deb -o cuda-keyring_1.1-1_all.deb \
     && dpkg -i cuda-keyring_1.1-1_all.deb \
@@ -23,11 +27,14 @@ function install_cuda_debian() {
     && curl -sSL "https://keyserver.ubuntu.com/pks/lookup?op=get&search=0xA4B469963BF863CC" | \
        gpg --dearmor | tee /etc/apt/keyrings/cuda.gpg > /dev/null \
     && apt-get update \
-    && apt-get install -y cuda-toolkit-12.9
+    && apt-get install -y \
+       cuda-minimal-build-${CUDA_PKG_VERSION} cuda-nvcc-${CUDA_PKG_VERSION} cuda-cudart-dev-${CUDA_PKG_VERSION}
 
     echo "[INFO] CUDA installation complete."
 }
 
+export PATH=/usr/local/cuda-${CUDA_VERSION}/bin${PATH:+:${PATH}}
+export LD_LIBRARY_PATH=/usr/local/cuda-${CUDA_VERSION}/lib64${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}
 
 if [ "x$(uname -m)" = "xx86_64" ]; then
     install_cuda_debian
@@ -41,3 +48,5 @@ if [ "x$(uname -m)" = "xx86_64" ]; then
 else
     echo "Unsupported platform for CUDA: ${TARGETPLATFORM}"
 fi
+
+exec "$@"
