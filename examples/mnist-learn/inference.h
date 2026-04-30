1de41f353957dd9bd9f0f727a918094d901940ed
Yuliya Z. Frolova <NA>
2026-04-30 19:08:45 +0300

    Doxygen and README fix

--- examples/mnist-learn/inference.h
+++ examples/mnist-learn/inference.h
@@ -46,7 +46,7 @@
  * and recording spike messages for analysis. It configures the network with appropriate input and output channels, sets up 
  * WTA mechanisms, initializes logging for spike monitoring, and executes the simulation.
  * 
- * @tparam Neuron neuron type template parameter for neuron model specification.
+ * @tparam Neuron neuron type for neuron model specification.
  * 
  * @param backend shared pointer to the computational backend for execution.
  * @param network annotated network structure containing the network and its annotations.
@@ -198,7 +198,7 @@ std::vector<knp::core::messaging::SpikeMessage> infer_network(
  * @details This function provides a high-level interface for running inference on a trained model, handling backend loading and 
  * delegation to the lower-level inference function.
  * 
- * @tparam Neuron neuron type template parameter for neuron model specification.
+ * @tparam Neuron neuron type for neuron model specification.
  * 
  * @param model_desc model description containing configuration parameters and paths.
  * @param dataset dataset with inference data.
