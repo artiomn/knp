8fc35dbf5cc8d5cae22b1d8edc0d1771b3d78edf
Artiom N. <artiom@artiom>
2026-07-30 16:03:48 +0300

    Common header implemented: #40

---
+++ knp/base-framework/include/knp/framework/knp.h
@@ -0,0 +1,33 @@
+/**
+ * @file knp.h
+ * @brief Common KNP header to include.
+ * @kaspersky_support Artiom N.
+ * @date 30.07.2026
+ * @license Apache 2.0
+ * @copyright © 2026 AO Kaspersky Lab
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+
+#include <knp/framework/backend_loader.h>
+#include <knp/framework/io/io.h>
+#include <knp/framework/logging.h>
+#include <knp/framework/model.h>
+#include <knp/framework/model_executor.h>
+#include <knp/framework/model_loader.h>
+#include <knp/framework/population/creators.h>
+#include <knp/framework/projection/connectors.h>
+#include <knp/framework/sonata/network_io.h>
+#include <knp/framework/tags/name.h>
